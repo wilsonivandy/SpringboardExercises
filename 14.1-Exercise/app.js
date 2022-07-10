2dbbf824ecca911c06d683d9e05fa4d2a0e963e0
wilsonivandy <wilsonivandy@gmail.com>
2022-07-10 10:30:08 +0800

    Finished Exercise 14.1

---
+++ 14.1-Exercise/app.js
@@ -0,0 +1,28 @@
+// What is a HTTP:
+// A HTTP (Hypertext Transfer Protocol) is an web protocol designed to manage the transfer of data, from and to, a server.
+
+// What is a URL:
+// A URL (Uniform Resource Locator) is an address to visit a specific web page
+
+// What is DNS:
+// DNS (Domain Name Service) is a system that converts web IP addresses into specific words in order to make them more readable. 
+
+// What is a query string:
+// A section of a URL that allows you to pass in key-value pairs
+
+// List two HTTP verbs and their use cases
+// GET: Get data from server
+// POST: Send data to server
+
+// What is an HTTP Request:
+// A Request made using the HTTP protocol
+
+// What is an HTTP Response:
+// A Response made using the HTTP protocol
+
+// What is an HTTP Header:
+// Provides information on whether it is a request or response
+
+// What are the processes that happen when you type a link into a browser?
+// Using DNS, browser resolves it into an IP address. Browser then makes a request to that IP address and the server sends a response back.
+// The browser then creates a DOM from the response HTML and loads any other resource received. Then browser makes unique HTTP requests for each of the resources. 
