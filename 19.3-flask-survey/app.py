a3b8e7e2f6d2dca5245d843e2086d18ef17b8882
wilsonivandy <wilsonivandy@gmail.com>
2022-08-01 09:46:44 +0700

    Updated 19.3 to finish 19.4 Exercise

--- 19.3-flask-survey/app.py
+++ 19.3-flask-survey/app.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
-
-responses = [] 
 
 app = Flask(__name__)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -14,21 +12,28 @@ debug = DebugToolbarExtension(app)
 def home():
     return render_template('home.html', title = satisfaction_survey.title, instructions = satisfaction_survey.instructions)
 
+@app.route('/start_survey', methods=["POST"])
+def start_survey():
+    session["responses"] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:number>')
 def question(number):
-    if len(satisfaction_survey.questions) == len(responses):
+    if len(satisfaction_survey.questions) == len(session["responses"]):
         return redirect('/thankyou')
-    elif number != len(responses):
+    elif number != len(session["responses"]):
         flash("You are trying to access an invalid question!")
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
     else:
-        return render_template("survey.html", question = satisfaction_survey.questions[number], currQuestion = number)
+        return render_template("survey.html", question = satisfaction_survey.questions[number], currQuestion = number, answered = len(session["responses"]))
 
 @app.route('/answer', methods=["POST"])
 def add_Response():
+    responses = session["responses"]
     response = request.form['response']
     responses.append(response)
-    nextIndex = len(responses)
+    session["responses"] = responses
+    nextIndex = len(session["responses"])
     if len(satisfaction_survey.questions) == nextIndex:
         return redirect('/thankyou')
     else: 
