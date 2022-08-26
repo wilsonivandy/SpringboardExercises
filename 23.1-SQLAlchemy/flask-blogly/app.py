01d8360a3f2601dea657defe47fbcd1f5bac10fe
wilsonivandy <wilsonivandy@gmail.com>
2022-08-26 15:51:38 +0800

    Finished 23.2 One-To-Many

--- 23.1-SQLAlchemy/flask-blogly/app.py
+++ 23.1-SQLAlchemy/flask-blogly/app.py
@@ -4,7 +4,7 @@ from contextlib import redirect_stderr
 from crypt import methods
 from email.mime import image
 from flask import Flask, render_template, session, request, redirect
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -16,6 +16,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+db.drop_all()
 db.create_all()
 
 
@@ -72,11 +73,60 @@ def userEditProcess(userId):
     return render_template('editUser.html', user=user)
 
 
+@app.route("/users/<int:userId>/posts/new")
+def addPost(userId):
+    user = User.query.get(userId)
+    return render_template('postForm.html', user=user)
+
+
+@app.route("/users/<int:userId>/posts/new", methods=['POST'])
+def addPostProcess(userId):
+    title = request.form['Title']
+    content = request.form['PostContent']
+
+    newPost = Post(title=title,
+                   content=content, user_id=userId)
+    db.session.add(newPost)
+    db.session.commit()
+    return redirect(f'/users/{userId}')
+
+
+@app.route("/posts/<int:postId>")
+def postInfo(postId):
+    post = Post.query.get(postId)
+    user = User.query.get(post.user_id)
+    return render_template('postInfo.html', post=post, user=user)
+
+
+@app.route("/posts/<int:postId>/edit")
+def postEdit(postId):
+    post = Post.query.get(postId)
+    user = User.query.get(post.user_id)
+    return render_template('editPost.html', post=post, user=user)
+
+
+@app.route("/posts/<int:postId>/edit", methods=['POST'])
+def postEditProcess(postId):
+    post = Post.query.get(postId)
+    post.title = request.form['Title']
+    post.content = request.form['PostContent']
+    db.session.commit()
+    return redirect(f'/users/{post.user_id}')
+
+
 @app.route("/users/<int:userId>/delete")
 def userDeleteProcess(userId):
     User.query.filter_by(id=userId).delete()
     db.session.commit()
     return redirect('/users')
+
+
+@app.route("/posts/<int:postId>/delete")
+def postDeleteProcess(postId):
+    post = Post.query.get(postId)
+    Post.query.filter_by(id=postId).delete()
+    db.session.commit()
+    return redirect(f'/users/{post.user_id}')
 
 
 @app.route("/reset")
--- 23.1-SQLAlchemy/flask-blogly/models.py
+++ 23.1-SQLAlchemy/flask-blogly/models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -20,12 +21,33 @@ class User(db.Model):
                    autoincrement=True)
     first_name = db.Column(db.String(50),
                            nullable=False,
-                           unique=True)
+                           unique=False)
     last_name = db.Column(db.String(50),
                           nullable=False,
-                          unique=True)
+                          unique=False)
     image_url = db.Column(db.String(50),
                           nullable=False)
 
     def greet(self):
         return f"Hi, my name is {self.first_name} {self.last_name}"
+
+    posts = db.relationship('Post', cascade="all, delete")
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id', ondelete="CASCADE"))
+    title = db.Column(db.String(50),
+                      nullable=False,
+                      unique=False)
+    content = db.Column(db.String(200),
+                        nullable=False,
+                        unique=False)
+    created_at = db.Column(db.DateTime)
+
+    user = db.relationship('User')
